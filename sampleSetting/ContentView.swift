d70bc576a8edb9ade09a2c3cc01c3adef8ef7e5e
iyong-il <lyi1233313@gmail.com>
2022-10-17 12:15:16 +0900

    22.10.187 / 에어플레인모드 수정

--- sampleSetting/ContentView.swift
+++ sampleSetting/ContentView.swift
@@ -8,13 +8,27 @@
 import SwiftUI
 
 struct ContentView: View {
+  @State var search = ""
   @State var toggleOn = false
+
 
   var body: some View {
     
+    
     NavigationView {
+      
       List {
+        HStack{
+          Image(systemName: "magnifyingglass")
+            .resizable()
+            .aspectRatio(contentMode: .fit)
+            .frame(width: 20, height: 20)
+            .foregroundColor(.gray)
+          TextField("검색", text: $search)
+            .keyboardType(.default)
+        }
         Section {
+          
           NavigationLink  {
             Text("프로필 화면")
           } label: {
@@ -38,34 +52,24 @@ struct ContentView: View {
             .padding(.vertical, 10)
           }
         }
-        Section {
-//          HStack {
-//            Image(systemName: "airplane")
-//              .resizable()
-//              .aspectRatio(contentMode: .fit)
-//              .frame(width: 20, height: 20)
-//              .padding(.all, 4)
-//              .background(.orange)
-//              .foregroundColor(.white)
-//              .cornerRadius(6)
-//            NavigationLink("에어플레인모드") {
-//              Text("에어플레인모드 화면")
-//            }
-//          }
 
+        Section {
+          // 에어플레인모드
           HStack {
+            Image(systemName: "airplane")
+              .resizable()
+              .aspectRatio(contentMode: .fit)
+              .frame(width: 20, height: 20)
+              .padding(.all, 4)
+              .background(.orange)
+              .foregroundColor(.white)
+              .cornerRadius(6)
+            Text("에어플레인모드")
             Toggle(isOn: self.$toggleOn) {
-              Image(systemName: "airplane")
-                .resizable()
-                .aspectRatio(contentMode: .fit)
-                .frame(width: 20, height: 20)
-                .padding(.all, 4)
-                .background(.orange)
-                .foregroundColor(.white)
-                .cornerRadius(6)
-              Text("에어플레인모드")
+
             }
           }
+          // 와이파이
           HStack {
             Image(systemName: "wifi")
               .resizable()
@@ -123,9 +127,24 @@ struct ContentView: View {
             }
           }
         }
+        
         Section {
           HStack {
             Image(systemName: "bell.badge.fill")
+              .resizable()
+              .aspectRatio(contentMode: .fit)
+              .frame(width: 20, height: 20)
+              .padding(.all, 4)
+              .background(.red)
+              .foregroundColor(.white)
+              .cornerRadius(6)
+
+            NavigationLink("알림") {
+              Text("알림 화면")
+            }
+          }
+          HStack {
+            Image(systemName: "speaker.wave.3.fill")
               .resizable()
               .aspectRatio(contentMode: .fit)
               .frame(width: 20, height: 20)
@@ -134,8 +153,36 @@ struct ContentView: View {
               .foregroundColor(.white)
               .cornerRadius(6)
 
-            NavigationLink("알림") {
-              Text("알림 화면")
+            NavigationLink("사운드 및 햅틱") {
+              Text("사운드 및 햅틱화면")
+            }
+          }
+          HStack {
+            Image(systemName: "moon.fill")
+              .resizable()
+              .aspectRatio(contentMode: .fit)
+              .frame(width: 20, height: 20)
+              .padding(.all, 4)
+              .background(.indigo)
+              .foregroundColor(.white)
+              .cornerRadius(6)
+
+            NavigationLink("집중모드") {
+              Text("집중모드 화면")
+            }
+          }
+          HStack {
+            Image(systemName: "hourglass")
+              .resizable()
+              .aspectRatio(contentMode: .fit)
+              .frame(width: 20, height: 20)
+              .padding(.all, 4)
+              .background(.indigo)
+              .foregroundColor(.white)
+              .cornerRadius(6)
+
+            NavigationLink("스크린 타임") {
+              Text("스크린타임 화면")
             }
           }
         }
@@ -151,13 +198,13 @@ struct ContentView: View {
               .cornerRadius(6)
             NavigationLink("일반") {
               List {
-              Section {
-                HStack {
-                  NavigationLink("정보") {
-                    Text("정보 화면")
+                Section {
+                  HStack {
+                    NavigationLink("정보") {
+                      Text("정보 화면")
+                    }
                   }
                 }
-              }
                 Section {
                   HStack {
                     NavigationLink("키보드") {
@@ -188,12 +235,51 @@ struct ContentView: View {
                 Section {
                   HStack {
                     NavigationLink("전송 또는 iPhone재설정") {
-                        Text("전송 또는 iPhone재설정 화면")
+                      Text("전송 또는 iPhone재설정 화면")
                     }
                   }
                 }
-            }
+              }
 
+            }
+          }
+          HStack {
+            Image(systemName: "switch.2")
+              .resizable()
+              .aspectRatio(contentMode: .fit)
+              .frame(width: 20, height: 20)
+              .padding(.all, 4)
+              .background(.gray)
+              .foregroundColor(.white)
+              .cornerRadius(6)
+            NavigationLink("제어센터") {
+              Text("제어센터 화면")
+            }
+          }
+          HStack {
+            Image(systemName: "textformat.size")
+              .resizable()
+              .aspectRatio(contentMode: .fit)
+              .frame(width: 20, height: 20)
+              .padding(.all, 4)
+              .background(.blue)
+              .foregroundColor(.white)
+              .cornerRadius(6)
+            NavigationLink("디스플레이 및 밝기") {
+              Text("디스플레이 및 밝기 화면")
+            }
+          }
+          HStack {
+            Image(systemName: "square.grid.4x3.fill")
+              .resizable()
+              .aspectRatio(contentMode: .fit)
+              .frame(width: 20, height: 20)
+              .padding(.all, 4)
+              .background(.blue)
+              .foregroundColor(.white)
+              .cornerRadius(6)
+            NavigationLink("홈 화면") {
+              Text("홈 화면의 화면")
             }
           }
           HStack {
